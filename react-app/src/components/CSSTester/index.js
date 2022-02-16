b3a191b07b0c0cf4e64d44bfaf188e2700613f3f
cat-friend <denisepli@gmail.com>
2022-02-16 09:11:23 -0500

    perfected the slanted div images

--- react-app/src/components/CSSTester/index.js
+++ react-app/src/components/CSSTester/index.js
@@ -1,6 +1,9 @@
+import headerbwd from '../../assets/header-bwd.svg'
+
 function CSSTester() {
     return (
         <div className="header-parent">
+            <img src={headerbwd} className="header-img"/>
             <div className="header-child">
                 <h2>HELLO WORLD</h2>
             </div>
