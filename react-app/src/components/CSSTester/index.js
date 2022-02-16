70588ebeb67c14647498085306d580f8aad4e19d
cat-friend <denisepli@gmail.com>
2022-02-16 11:13:59 -0500

    made header more responsive

--- react-app/src/components/CSSTester/index.js
+++ react-app/src/components/CSSTester/index.js
@@ -1,12 +1,18 @@
+import headerfwd from '../../assets/header-fwd-small.svg'
 import headerbwd from '../../assets/header-bwd.svg'
+import headertest from '../../assets/header-test.svg'
 
 function CSSTester() {
     return (
         <div className="header-parent">
-            <img src={headerbwd} className="header-img"/>
-            <div className="header-child">
-                <h2>HELLO WORLD</h2>
-            </div>
+            {/* <img src={headerfwd} className="header-img" /> */}
+            {/* <img src={headerbwd} className="header-img" /> */}
+            {/* <img src={headertest} className="header-img" /> */}
+            {/* <img src="https://denisepli.com/wildr/images/31.JPG" className="header-img" /> */}
+            {/* <div className="header-child">
+
+            </div> */}
+            <h2>HELLO WORLD</h2>
         </div>
     )
 
