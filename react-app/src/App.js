d485deb53ccbe9f46ec5f9078c4b0174b7343620
cat-friend <denisepli@gmail.com>
2022-02-16 08:30:46 -0500

    added fonts, div background image, styled h2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import CSSTester from './components/CSSTester';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -28,6 +29,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path='/' exact={true}>
+          <CSSTester />
         </Route>
         <Route path='/sign-up' exact={true}>
           <SignUpForm />
---
+++ react-app/src/components/CSSTester/index.js
@@ -0,0 +1,12 @@
+function CSSTester() {
+    return (
+        <div className="header-parent">
+            <div className="header-child">
+                <h2>HELLO WORLD</h2>
+            </div>
+        </div>
+    )
+
+
+}
+export default CSSTester
