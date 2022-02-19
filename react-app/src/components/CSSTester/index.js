2fb1ba7bfaebd4c2c4c5af1dcf4a168fd121b471
cat-friend <denisepli@gmail.com>
2022-02-18 23:40:40 -0500

    css tweaking

--- react-app/src/components/CSSTester/index.js
+++ react-app/src/components/CSSTester/index.js
@@ -4,9 +4,32 @@ import headertest from '../../assets/header-test.svg'
 
 function CSSTester() {
     return (
-        <div className="header-parent">
-            <h2>HELLO WORLD</h2>
-        </div>
+        <>
+            <div className="header-parent">
+                <div className="left-corner"></div>
+                <div className="header-child"><h2>HELLO WORLD</h2></div>
+                <div className="right-corner"></div>
+            </div>
+            <div className="content-container">
+                <div className="content">
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                    <p>HELLO</p>
+                </div>
+            </div>
+        </>
     )
 
 
