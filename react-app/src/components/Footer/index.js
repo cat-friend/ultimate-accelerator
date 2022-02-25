c31643ec87637dc975e3ed7d4778e7ecca8dfdfc
cat-friend <denisepli@gmail.com>
2022-02-25 10:11:09 -0500

    added links to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -4,9 +4,9 @@ function Footer() {
     return (
         <div id="footer">
             <div id="bio-links">
-                <div>Denise Li</div>
-                <div>GitHub</div>
-                <div>LinkedIn</div>
+                <div><a href="mailto:denisepli@gmail.com">Denise Li</a></div>
+                <div><a href="https://github.com/cat-friend/ultimate-accelerator">GitHub</a></div>
+                <div><a href="https://www.linkedin.com/in/denise-li-45350320/" target="_blank">LinkedIn</a></div>
             </div>
             <div id="tech">
                 <div>Python</div>
@@ -22,6 +22,8 @@ function Footer() {
                 <div>SQLAlchemy</div>
                 <div>|</div>
                 <div>WTForms</div>
+                <div>|</div>
+                <div>PostgreSQL</div>
                 <div>|</div>
                 <div>HTML/CSS</div>
                 <div>|</div>
