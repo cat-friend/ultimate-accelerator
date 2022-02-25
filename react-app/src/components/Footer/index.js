1013c4f565222ce46fd54902c1d3b01c42a1aa95
cat-friend <denisepli@gmail.com>
2022-02-25 10:12:46 -0500

    added target='_blank' t o footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
         <div id="footer">
             <div id="bio-links">
                 <div><a href="mailto:denisepli@gmail.com">Denise Li</a></div>
-                <div><a href="https://github.com/cat-friend/ultimate-accelerator">GitHub</a></div>
+                <div><a href="https://github.com/cat-friend/ultimate-accelerator" target="_blank">GitHub</a></div>
                 <div><a href="https://www.linkedin.com/in/denise-li-45350320/" target="_blank">LinkedIn</a></div>
             </div>
             <div id="tech">
