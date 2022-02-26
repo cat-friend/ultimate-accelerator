ff6762a1ce9c51bd1e446703e294d5581eb8a30a
cat-friend <denisepli@gmail.com>
2022-02-26 11:55:19 -0500

    styled footed

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,30 +3,26 @@ import "./Footer.css";
 function Footer() {
     return (
         <div id="footer">
-            <div id="bio-links">
-                <div><a href="mailto:denisepli@gmail.com">Denise Li</a></div>
-                <div><a href="https://github.com/cat-friend/ultimate-accelerator" target="_blank">GitHub</a></div>
-                <div><a href="https://www.linkedin.com/in/denise-li-45350320/" target="_blank">LinkedIn</a></div>
+            <div id="skewed">
+                <div id="bio-links">
+                    <div><a href="mailto:denisepli@gmail.com">Denise Li</a></div>
+                    <div><a href="mailto:denisepli@gmail.com">Email</a></div>
+                    <div><a href="https://github.com/cat-friend/ultimate-accelerator" target="_blank">GitHub</a></div>
+                    <div><a href="https://www.linkedin.com/in/denise-li-45350320/" target="_blank" id="end">LinkedIn</a></div>
+                </div>
             </div>
-            <div id="tech">
+            <div
+             id="tech">
+                 <div>Tech stack:</div>
                 <div>Python</div>
-                <div>|</div>
                 <div>JavaScript</div>
-                <div>|</div>
                 <div>ReactJS</div>
-                <div>|</div>
                 <div>NodeJS</div>
-                <div>|</div>
                 <div>Flask</div>
-                <div>|</div>
                 <div>SQLAlchemy</div>
-                <div>|</div>
                 <div>WTForms</div>
-                <div>|</div>
                 <div>PostgreSQL</div>
-                <div>|</div>
                 <div>HTML/CSS</div>
-                <div>|</div>
                 <div>Deployed with Docker</div>
             </div>
         </div>
