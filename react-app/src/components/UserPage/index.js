fe13b7ef3a739207ff491dc42eecff1f896cfa6f
cat-friend <denisepli@gmail.com>
2022-03-09 08:14:28 -0500

    added all data-grabbing and state-subscribing funtions to userpage

--- react-app/src/components/UserPage/index.js
+++ react-app/src/components/UserPage/index.js
@@ -1,3 +1,16 @@
-import { useState } from "react"
+import { useEffect } from "react"
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { getOneUser } from "../../store/user";
+
+function UserPage(){
+    const {userId} = useParams();
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const user = useSelector(state => state.user);
+    useEffect(() => {
+        dispatch(getOneUser(userId));
+    }, [dispatch, userId]);
+}
+
+export default UserPage;
