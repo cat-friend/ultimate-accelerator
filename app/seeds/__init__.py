604bbe948548c94540e74c0898c19bd1794f6d39
cat-friend <denisepli@gmail.com>
2022-03-10 13:34:18 -0500

    combined seed and import functions into one to make code DRYer

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -31,7 +31,8 @@ def seed():
     seed_clans()
     seed_clan_users()
     seed_messages()
-    seed_user_challenge()
+    seed_one_user([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                   16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
 
 
 @seed_commands.command('undo')
@@ -72,4 +73,5 @@ def reset():
     seed_clans()
     seed_clan_users()
     seed_messages()
-    seed_user_challenge()
+    seed_one_user([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                   16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
